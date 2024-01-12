fd8b05044c45a6774c9b677a9001ba048e071780
Rick <88679@glr.nl>
2024-01-12 12:25:55 +0100

    changes

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uploadify</title>
+    <link rel="stylesheet" href="./css/main-style.css">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/footer.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <nav-container1>
+                <h1>Uploadify</h1>
+            </nav-container1>
+
+            <nav-container3>
+                <img src="" alt="LOGO">
+            </nav-container3>
+
+            <nav-container2>
+                <h4>Main</h4>
+                <h4>Login</h4>
+                <h4>Logout</h4>
+            </nav-container2>
+        </nav>
+    </header>
+    <main>
+        <input type="file" multiple>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
